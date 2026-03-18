1141d902c6387e9036e630e7b195638194adf488
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-03-18 19:42:10 +0100

    intial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import AdminInfo from "./components/AdminInfo";
-import UserInfo from "./components/UserInfo";
-import { Info, AdminInfoList } from "./types";
+import Counter from "./components/Counter";
 
 const App = () => {
-  const user: Info = {
-    id: 1,
-    name: "John Doe",
-    email: "john@gmail.com",
-  };
-
-  const admin: AdminInfoList = {
-    id: 2,
-    name: "Jane Smith",
-    email: "jane@gmail.com",
-    role: "admin",
-    lastLogin: new Date(),
-  };
-
   return (
     <div>
-      <UserInfo user={user} />
-      <AdminInfo admin={admin} />
+      <Counter />
     </div>
   );
 };
