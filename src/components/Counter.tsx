e83a0c38936c00e56300047cd61c57fa47518c42
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-03-19 14:47:28 +0100

    use reducer for counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,27 @@
-import { useContext, type FC } from "react";
-import { MyContext } from "./MyContext";
+import React, { useReducer } from "react";
 
-const Counter: FC = () => {
-  const { count, increment, decrement } = useContext(MyContext);
+type State = { count: number };
+type Action = { type: "INCREMENT" } | { type: "DECREMENT" };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+
+      return state;
+  }
+};
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
     </div>
   );
 };
